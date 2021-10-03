cb8d0ef7ebca524cf94c533a39a8f0e675151b6f
Egor <paradize9140@gmail.com>
2021-10-03 19:28:14 +0200

    first commit

---
+++ app2.js
@@ -0,0 +1,18 @@
+// alert(1);
+
+// const result = confirm("are you here?")
+
+// console.log(result);
+
+// const answer = +prompt('Вам есть 18&', '18'); с помощью плюса перед prompt можно вывести не строковое значение и числовое
+
+// console.log(typeof(answer));
+
+const answers = [];
+
+answers[0] = prompt('Как ваше имя?', '');
+answers[1] = prompt('Как ваше фамилия?', '');
+answers[2] = prompt('Сколько вам лет?', '');
+
+document.write(answers);
+
---
+++ app3.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const category = 'toys';
+
+console.log(`https://someurl.come/${category}/5`);
+
+const user = "Egor";
+
+alert(`Привет, ${user}`);
